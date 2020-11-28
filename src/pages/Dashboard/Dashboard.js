6e87899ca09df21519ca9df97f6dfd204420d849
Utkarsh Dubey <utkarsh.dubey3jan@gmail.com>
2020-11-28 20:26:06 +0530

    Added netlify _redirects

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -21,40 +21,35 @@ export default function Dashboard() {
         <Loading />
       ) : (
         <Layout auth={true}>
-            <section className="hero__bg">
-              <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                <div className="text-center lg:w-2/3 w-full">
-                  <h1
-                    className="w-128 sm:text-5xl mb-4 font-black text-primaryIndigo font-inter"
-                    style={{ fontSize: 90 }}
-                  >
-                    Hi, <span className="text-primary">{user.name}</span>.
-                  </h1>
-                  <div className="container px-5 mx-auto my-12">
-                    <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center">
-                      <h1 className="hero__content-block--title font-vg5000 text-center">
-                        Select events you are 
-                        <span className="bg-yellow-300 px-1 m-1">
-                          {" "}
-                          interested{" "}
-                        </span>
-                        in:
-                      </h1>
-                    </div>
+          <section className="hero__bg">
+            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+              <div className="text-center lg:w-2/3 w-full">
+                <h1
+                  className="w-128 sm:text-5xl mb-4 font-black text-primaryIndigo font-inter"
+                  style={{ fontSize: 90 }}
+                >
+                  Hi, <span className="text-primary">{user.name}</span>.
+                </h1>
+                <div className="container px-5 mx-auto my-12">
+                  <div className="lg:w-4/5 mx-auto flex flex-wrap justify-center">
+                    <h1 className="hero__content-block--title font-vg5000 text-center">
+                      Select events you are
+                      <span className="bg-yellow-300 px-1 m-1">
+                        {" "}
+                        interested{" "}
+                      </span>
+                      in:
+                    </h1>
                   </div>
                 </div>
               </div>
-            </section>
-            
-          <section id="hero__content-block" className="px-18"></section>
-          <section className="hero__quote-block bg-primary p-32">
+            </div>
+          </section>
+          <section className="hero__prelimlink-block bg-primary p-16">
             <div className="flex justify-center align-center flex-col">
-              <h1 className="text-center font-vg5000 text-4xl text-white">
-                ~ Kutteshna kutteshna kutteshna kutteshna kutteshna kutteshna. ~
+              <h1 className="text-center font-vg5000 text-2xl text-white">
+                Click here to view preliminary prompts
               </h1>
-              <p className="text-white text-center text-white font-vg5000 text-2xl pt-6">
-                ~ Utkarsh the son of God
-              </p>
             </div>
           </section>
         </Layout>
