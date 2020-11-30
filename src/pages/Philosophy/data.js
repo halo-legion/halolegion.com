66cb4ba8994e3348e4de135ed6cec76bfc789bda
udxx <69747962+udxx@users.noreply.github.com>
2020-11-30 16:29:16 +0530

    update

--- src/pages/Philosophy/data.js
+++ src/pages/Philosophy/data.js
@@ -2,22 +2,31 @@ export const philosophyData = [
   {
     name: "Solidarity",
     color: "#70A1FF",
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    description: `Solidarity (noun) - unity or agreement of feeling or action, especially among individuals with a common interest; mutual support within a group.
+“There is no stability without solidarity and no solidarity without stability.” We believe that solidarity is essential for mutual aid; supportive activity towards other people does not result from the expectation of reward, but rather from instinctive feelings of solidarity. 
+.`,
   },
   {
     name: "Inquisitiveness",
     color: "#7BED9F",
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    description: `Inquisitiveness(noun) - the quality of wanting to discover as much as you can about things.
+We believe that the core of great ideas and inventions lie in the quality of being inquisitive. We don't define success as  the outcome, we define it as having the courage to start the process and everything the process gives you.
+`,
   },
   {
     name: "Learn as you create",
     color: "#FF6B81",
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    description: `“You grow old when you stop learning.”
+We believe in the process of learning through creating. You don’t need to know everything about tech, but you need to have the curiosity to learn.  We believe that knowledge is a big thing but if practice is not done, knowledge cannot take us to our goal. We have to convert our knowledge into action which needs regular creation.
+.`,
   },
   {
     name: "Co-operation",
     color: "#CD84F1",
-    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+    description: `Co-operation(noun)- the action or process of working together to the same end.
+“Teamwork makes the dream work” 
+Aristotle defined man to be a social animal by nature. He cannot survive without working with others toward a common aim. The prime reason to cooperate in a working environment is to achieve synergy. We believe that cooperation is the pillar of teamwork and has to be a part of any team that hopes to be successful. The two most important ingredients to get along in this world are sharing and taking turns. We believe in taking time to take turns, to learn from each other. 
+`,
   },
   
 ];
