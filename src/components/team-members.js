587015dcd10790b3aa9a71540eb6101c9f9ff84d
Ashutosh Singh <63228537+AshutoshSingh9@users.noreply.github.com>
2020-12-01 08:51:57 +0530

    Hariom
    Team-member-pics

--- src/components/team-members.js
+++ src/components/team-members.js
@@ -12,7 +12,7 @@ When I'm not writing code, I enjoy writing stories on Medium, poems, articles, a
     socials: {
       instagram: "https://www.instagram.com/youteakay/",
       twitter: "https://twitter.com",
-      github: "https://github.com",
+      github: "https://github.com/utkarshdubey",
     },
   },
   {
@@ -21,7 +21,7 @@ When I'm not writing code, I enjoy writing stories on Medium, poems, articles, a
     description: `I’m an enthusiastic writer and am looking forward to working on the Halo magazine along with being the Head of Writing/Pitching. I strive to improve myself every day by exploring new concepts and reflecting on myself. When I’m not pitching new ideas, I love to read and cook.
 I'm interested in pursuing a deeper knowledge of behavioral economics, computer science, and business.
 Currently, I’m a content writer at an economics club, doing research on behavioral economics, working with the local government hospital, indulging in various competitions, and trying to keep up with my studies.`,
-    image: "https://dummyimage.com/400x400",
+    image: "blob:https://imgur.com/31e43c20-0f3a-47a9-b96a-a3260329564c",
     socials: {
       instagram: "https://www.instagram.com/__udeshna__/",
       twitter: "https://twitter.com",
@@ -36,11 +36,11 @@ Currently, I’m a content writer at an economics club, doing research on behavi
 I am an avid web developer based out of Indore, India. I work on creating better stuff out of your everyday tech. Currently flicking headshots and preparing for SAT ‘21. Also reliving the fond memories as EXUN 2020 winner. Right Now Working as an Independent Contractor. If you like my work find me here or you can always shoot me an email. 
 
 This year just two things PETE CONRAD and being gaming head, Team Halo.`,
-    image: "https://dummyimage.com/400x400",
+    image: "https://i.imgur.com/p6cRWsz.png",
     socials: {
       instagram: "https://www.instagram.com/ashutosh.singh09_/",
       twitter: "https://twitter.com",
-      github: "https://github.com",
+      github: "https://github.com/AshutoshSingh9",
     },
   },
   {
@@ -49,7 +49,7 @@ This year just two things PETE CONRAD and being gaming head, Team Halo.`,
     description: `I’m a creative and talented Graphic Designer with extensive experience in design. I’ve worked in Indore since 2012 and specialize in creating unique graphics to boost your brand, as well as visual media that will help set you apart from your competition.With a commitment to my clients and an extensive skill set, which I am always developing, I always strive to exceed expectations. Contact me to find out how I can create a design with high visual impact to generate the results you want.`,
     image: "https://dummyimage.com/400x400",
     socials: {
-      instagram: "https://www.instagram.com/deadzolt/",
+      instagram: "https://i.imgur.com/1VnK94s.png",
       twitter: "https://twitter.com",
       github: "https://github.com",
     },
